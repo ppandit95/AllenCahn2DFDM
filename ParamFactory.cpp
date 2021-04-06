0c9cb9dc623f2d3d98550b40646fe736e1039322
Pushkar A Pandit <pushkarpandit95@gmail.com>
2021-04-06 19:55:22 +0530

    Created a class to parse the parameters

--- ParamFactory.cpp
+++ ParamFactory.cpp
@@ -10,48 +10,46 @@
 #include<config4cpp/Configuration.h>
 #include<vector>
 #include<string>
+#include"ParamFactory.hpp"
+#include"structures.hpp"
 
 using namespace config4cpp;
 
 
 
-int main(int argc,char** argv){
-	Configuration * cfg = Configuration::create();
-	const char* scope = "";
-	const char* configFile = "parameters.cfg";
-	int Nx;
-	int Ny;
-	int dx;
-	int dy;
-	int radius;
-	double TimeStep;
-	int FinalTime;
-	StringVector Steps;
-	std::vector<int> steps;
-
-
-		try{
-			cfg->parse(configFile);
-			Nx = cfg->lookupInt(scope,"Nx");
-			Ny = cfg->lookupInt(scope,"Ny");
-			dx = cfg->lookupInt(scope,"dx");
-			dy = cfg->lookupInt(scope,"dy");
-			radius = cfg->lookupInt(scope,"radius");
-			TimeStep = cfg->lookupFloat(scope, "TimeStep");
-			FinalTime = cfg->lookupInt(scope, "FinalTime");
-			cfg->lookupList(scope, "steps",Steps);
-			for(unsigned int i=0;i<Steps.length();i++)
-				steps.push_back(atoi(Steps[i]));
-
+void ParamFactory::ParseParams(Parameters& param){
+	 cfg = Configuration::create();
+	 scope = "";
+	 configFile = "parameters.cfg";
+	try{
+		cfg->parse(configFile);
+		param.Nx = cfg->lookupInt(scope,"Nx");
+		param.Ny = cfg->lookupInt(scope,"Ny");
+		param.dx = cfg->lookupInt(scope,"dx");
+		param.dy = cfg->lookupInt(scope,"dy");
+		param.radius = cfg->lookupInt(scope,"radius");
+		param.TimeStep = cfg->lookupFloat(scope, "TimeStep");
+		param.FinalTime = cfg->lookupInt(scope, "FinalTime");
+		cfg->lookupList(scope, "steps",Steps);
+		for(unsigned int i=0;i<Steps.length();i++)
+			param.steps.push_back(atoi(Steps[i]));
 		}
 		catch(const ConfigurationException& ex){
 			std::cerr<<ex.c_str()<<std::endl;
 			cfg->destroy();
-			return 1;
 		}
-		std::cout<<"The parameters are"<<Nx<<","<<Ny<<","<<dx<<","<<dy<<","<<radius<<","<<TimeStep<<","<<FinalTime<<","<<steps[0]<<std::endl;
 		cfg->destroy();
-		return 0;
+
+}
+int main(int argc,char** argv){
+	ParamFactory pf;
+	Parameters par;
+	pf.ParseParams(par);
+	std::cout<<"The parameters are"<<par.Nx<<","
+			<<par.Ny<<","<<par.dx<<","<<par.dy<<","
+			<<par.radius<<","<<par.TimeStep<<","<<par.FinalTime
+			<<","<<par.steps[2]<<std::endl;
+
 }
 
 
