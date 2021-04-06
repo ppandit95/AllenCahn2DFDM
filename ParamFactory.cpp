183f5228032041caf78f4ebb1672ee524ada3e4d
Pushkar A Pandit <pushkarpandit95@gmail.com>
2021-04-06 18:40:34 +0530

    Written function to parse input but not yet tested

--- ParamFactory.cpp
+++ ParamFactory.cpp
@@ -5,53 +5,53 @@
  *      Author: pushkar
  */
 
-#include<iostream>
-#include<locale.h>
-#include<config4cpp/Configuration.h>
 #include<vector>
 #include<string>
-
+#include"config4cpp/Configuration.h"
+#include"ParamFactory.hpp"
+#include"structures.hpp"
 using namespace config4cpp;
 
+void ParamFactory::ParseParams(Parameters& param){
+	cfg = Configuration::create();
+	scope = "";
+	configFile = "parameters.cfg";
+	try{
+		cfg->parse(configFile);
+		Nx = cfg->lookupInt(scope,"Nx");
+		Ny = cfg->lookupInt(scope,"Ny");
+		dx = cfg->lookupInt(scope,"dx");
+		dy = cfg->lookupInt(scope,"dy");
+		radius = cfg->lookupInt(scope,"radius");
+		TimeStep = cfg->lookupFloat(scope,"TimeStep");
+		FinalTime = cfg->lookupInt(scope,"FinalTime");
+		cfg->lookupList(scope,"steps",Steps);
+		for(unsigned int i=0;i<steps.length();i++)
+			steps[i] = atoi(Steps[i]);
 
-
-int main(int argc,char** argv){
-	Configuration * cfg = Configuration::create();
-	const char* scope = "";
-	const char* configFile = "parameters.cfg";
-	int Nx;
-	int Ny;
-	int dx;
-	int dy;
-	int radius;
-	double TimeStep;
-	int FinalTime;
-	StringVector Steps;
-	std::vector<int> steps;
-
-
-		try{
-			cfg->parse(configFile);
-			Nx = cfg->lookupInt(scope,"Nx");
-			Ny = cfg->lookupInt(scope,"Ny");
-			dx = cfg->lookupInt(scope,"dx");
-			dy = cfg->lookupInt(scope,"dy");
-			radius = cfg->lookupInt(scope,"radius");
-			TimeStep = cfg->lookupFloat(scope, "TimeStep");
-			FinalTime = cfg->lookupInt(scope, "FinalTime");
-			cfg->lookupList(scope, "steps",Steps);
-			for(unsigned int i=0;i<Steps.length();i++)
-				steps.push_back(atoi(Steps[i]));
-
-		}
-		catch(const ConfigurationException& ex){
-			std::cerr<<ex.c_str()<<std::endl;
-			cfg->destroy();
-			return 1;
-		}
-		std::cout<<"The parameters are"<<Nx<<","<<Ny<<","<<dx<<","<<dy<<","<<radius<<","<<TimeStep<<","<<FinalTime<<","<<steps[0]<<std::endl;
+		tau = cfg->lookupFloat(scope,"tau");
+		gamma = cfg->lookupFloat(scope,"gamma");
+		epsilon = cfg->lookupFloat(scope,"epsilon");
+		L = cfg->lookupFloat(scope,"L");
+	}catch(const ConfigurationException& ex){
+		std::cerr<<ex.c_str()<<std::endl;
 		cfg->destroy();
-		return 0;
+	}
+	param.Nx = Nx;
+	param.Ny = Ny;
+	param.dx = dx;
+	param.dy = dy;
+	param.radius = radius;
+	param.TimeStep = TimeStep;
+	param.FinalTime = FinalTime;
+	for(unsigned int i=0;i<steps.length();i++)
+		param.steps_param[i] = steps[i];
+
+	param.tau = tau;
+	param.gamma = gamma;
+	param.epsilon = epsilon;
+	param.L = L;
+	cfg->destroy();
 }
 
 
